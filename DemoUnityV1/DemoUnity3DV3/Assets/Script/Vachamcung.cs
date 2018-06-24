99f3008b2df21322f5ee4283128d0b69dc09b592
tuanlinh3667 <linhvtd00569@fpt.edu.vn>
2018-06-24 16:16:45 +0700

    Create Bale Jump and Destroy Plane use AddFogre, Destroy 2

---
+++ DemoUnityV1/DemoUnity3DV3/Assets/Script/Vachamcung.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vachamcung : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("This is start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("This is update");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Start collide");
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Being collide");
+        Rigidbody rigi = GetComponent<Rigidbody>();
+        rigi.AddForce(transform.up * 250);
+        rigi.useGravity = true;
+        Destroy(collision.gameObject, 0.8f);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit collide");
+    }
+}   
