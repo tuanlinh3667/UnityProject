3d0a3c6fba807f6768961cf58470be214bf7e6c5
tuanlinh3667 <linhvtd00569@fpt.edu.vn>
2018-06-24 15:31:32 +0700

    Create Bale collide Plane use Destroy 1

---
+++ DemoUnityV1/DemoUnity3DV2/Assets/Script/Vachamcung.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vachamcung : MonoBehaviour {
+    public GameObject obj;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("This is start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("This is update");
+    }
+
+    void OnCollisionEnter(Collision collide)
+    {
+        Debug.Log("This is start collide!");
+    }
+
+    void OnCollisionStay(Collision collide)
+    {
+        if (collide.gameObject.name == "Matphang2")
+        {
+            obj = GameObject.Find("Matphang2");
+            Destroy(obj);
+        }
+        else
+        {
+            obj = GameObject.Find("Matphang4");
+            Destroy(obj, 3f);
+        }
+        
+    }
+
+    void OnCollisionExit(Collision collide)
+    {
+        Debug.Log("Exit collide!");
+    }
+}
---
+++ DemoUnityV1/DemoUnity3DV2/Assets/Script/Vachammem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vachammem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("This is start!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("This is update");
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("This is start collide!");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("This is be colliding!");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit collide!");
+    }
+}
